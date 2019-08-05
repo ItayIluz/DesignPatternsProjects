a6b6420e8ce05368ddb9417242e3358ed93b303c
ItayIluz <42839104+ItayIluz@users.noreply.github.com>
2019-08-05 21:32:05 +0300

    Changed panel

--- DP_Ex01/DP_Ex01/WordUsageDataPanel.cs
+++ DP_Ex01/DP_Ex01/WordUsageDataPanel.cs
@@ -5,18 +5,6 @@ namespace DP_Ex01
 {
     public class WordUsageDataPanel : Panel
     {
-        List<string> m_Posts;
-
-        public List<string> Posts
-        {
-            get
-            {
-                return m_Posts;
-            }
-            set
-            {
-                m_Posts = value;
-            }
-        }
+        public List<string> Posts { get; set; }
     }
 }
